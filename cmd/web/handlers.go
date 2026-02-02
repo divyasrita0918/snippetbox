41548dbcc24c6b40092226438c804258a5f3d651
divyasrita0918 <tpdivyasrita10@gmail.com>
2026-02-02 19:09:39 +0530

    Structured logging

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,4 +1,5 @@
 package main
+
 import (
     "fmt"
     "html/template"
@@ -6,21 +7,32 @@ import (
     "net/http"
     "strconv"
 )
+
+
 func home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
-    ts, err := template.ParseFiles(".ui/html/pages/home.tmpl")
+    
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/partials/nav.tmpl",
+        "./ui/html/pages/home.tmpl",
+    }
+
+    ts, err := template.ParseFiles(files...)
     if err != nil{
         log.Print(err.Error())
         http.Error(w, "Internal Server Error" , http.StatusInternalServerError)
         return
     }
 
-    err = ts.Execute(w, nil)
+    err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
         log.Print(err.Error())
         http.Error(w, "Internal Server Error", http.StatusInternalServerError)
     }
 }
+
+
 func snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.PathValue("id"))
     if err != nil || id < 1 {
@@ -29,9 +41,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
     }
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
+
+
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Display a form for creating a new snippet..."))
 }
+
+
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusCreated)
     w.Write([]byte("Save a new snippet..."))
