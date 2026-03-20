04ccd29d040306ec08c95d836d1ecabae6e795cf
divyasrita0918 <tpdivyasrita10@gmail.com>
2026-03-20 16:52:30 +0530

    Auto displaying flash messages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -30,24 +30,22 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	snippet, err := app.snippets.Get(id)
-	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			http.NotFound(w, r)
-		} else {
-			app.serverError(w, r, err)
-		}
-		return
-	}
-	flash := app.sessionManager.PopString(r.Context(), "flash")
-	data := app.newTemplateData(r)
-	data.Snippet = snippet
-	data.Flash = flash
-	app.render(w, r, http.StatusOK, "view.tmpl", data)
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+    snippet, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            http.NotFound(w, r)
+        } else {
+            app.serverError(w, r, err)
+        }
+        return
+    }
+    data := app.newTemplateData(r)
+    data.Snippet = snippet
+    app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
