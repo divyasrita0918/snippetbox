a63d5f744081106dfec4b8229dfd60a7947f009f
divyasrita0918 <tpdivyasrita10@gmail.com>
2026-03-21 14:25:00 +0530

    Signup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls" 
 	"database/sql"
 	"flag"
 	"html/template"
@@ -19,6 +20,7 @@ import (
 type application struct {
 	logger         *slog.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
@@ -26,41 +28,50 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
-	flag.Parse()
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	db, err := openDB(*dsn)
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
-
-	defer db.Close()
-	templateCache, err := newTemplateCache()
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
-
-	formDecoder := form.NewDecoder()
-
-	sessionManager := scs.New()
-	sessionManager.Store = mysqlstore.New(db)
-	sessionManager.Lifetime = 12 * time.Hour
-
-	app := &application{
-		logger:         logger,
-		snippets:       &models.SnippetModel{DB: db},
-		templateCache:  templateCache,
-		formDecoder:    formDecoder,
-		sessionManager: sessionManager,
-	}
-
-	logger.Info("starting server", "addr", *addr)
-
-	err = http.ListenAndServe(*addr, app.routes())
-	logger.Error(err.Error())
-	os.Exit(1)
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+    flag.Parse()
+    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+    db, err := openDB(*dsn)
+    if err != nil {
+        logger.Error(err.Error())
+        os.Exit(1)
+    }
+    defer db.Close()
+    templateCache, err := newTemplateCache()
+    if err != nil {
+        logger.Error(err.Error())
+        os.Exit(1)
+    }
+    formDecoder := form.NewDecoder()
+    sessionManager := scs.New()
+    sessionManager.Store = mysqlstore.New(db)
+    sessionManager.Lifetime = 12 * time.Hour
+    sessionManager.Cookie.Secure = true
+    app := &application{
+        logger:         logger,
+        snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
+        templateCache:  templateCache,
+        formDecoder:    formDecoder,
+        sessionManager: sessionManager,
+    }
+	tlsConfig := &tls.Config{
+        CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+    }
+    srv := &http.Server{
+        Addr:     *addr,
+        Handler:  app.routes(),
+        ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig: tlsConfig,
+		IdleTimeout:  time.Minute,
+        ReadTimeout:  5 * time.Second,
+        WriteTimeout: 10 * time.Second,
+    }
+    logger.Info("starting server", "addr", srv.Addr)
+    
+    err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+    logger.Error(err.Error())
+    os.Exit(1)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
@@ -75,3 +86,5 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 	return db, nil
 }
+
+
